fa36afe94e68fca3feae15da6b0e08dac23c3259
Asadujjaman Julhas <fahim0760@gmail.com>
2022-02-17 03:00:03 +0600

    complete responsive part

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,10 +18,12 @@ function checkEmptyValue(id) {
 // Check input filed is not empty
 function checkIncomeBalane() {
   const balanceValue = getById("income").value;
-  if(balanceValue == '') {
+  if (balanceValue == '') {
     getById("expance_cal").disabled = true;
+    getById("saving_btn").disabled = true;
   } else {
     getById("expance_cal").disabled = false;
+    getById("saving_btn").disabled = false;
   }
 }
 checkIncomeBalane()
@@ -45,11 +47,35 @@ getById("expance_cal").addEventListener(
       getById("balance").innerHTML = `<span class="waring_alert">You expense more than your income</span>`;
       getById("savings").style.display = "none";
       getById("total_ammount").style.display = "none";
+      getById("income").style.border = "1px solid #ff0000";
     } else {
       getById("balance").innerText = nowBalance;
       getById("savings").style.display = "block";
       getById("total_ammount").style.display = "block";
+      getById("income").style.border = "1px solid #ced4da";
     }
-    //console.log(totalExpenses)
+    getById("remaining_balance").innerText = '';
+    getById("total_saving").innerText = '';
+
   }
 );
+
+// Savings and total balance
+getById("saving_btn").addEventListener(
+  "click",
+  function () {
+    // Get my incoam
+    const myIncome = getById("income").value;
+    const savingPercent = getById("saving_percent").value;
+    const savingAmmount = myIncome / 100 * savingPercent;
+    const remainingBalance = getById("balance").innerText - savingAmmount;
+    if (remainingBalance < 0) {
+      getById("total_saving").innerHTML = `<span class="waring_alert">Don't able to save ${savingPercent}% money.</span>`;
+      getById("remaining_balance").innerText = '';
+      getById("saving_percent").style.border = "1px solid #ff0000";
+    } else {
+      getById("total_saving").innerText = savingAmmount;
+      getById("remaining_balance").innerText = remainingBalance;
+      getById("saving_percent").style.border = "1px solid #ced4da";
+    }
+  })
